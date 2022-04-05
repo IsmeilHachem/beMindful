1f69b7449963d004cd476200ebd9a4719aa242ab
Ismeil Hachem <ismeil27@gmail.com>
2022-04-04 23:24:22 -0400

    Created quick little motivation

---
+++ index.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+
+const greeting = prompt("Hello shithead, what do you want? ");
+console.log(`Two tickets to ${greeting}`);
+
+console.log("\nEveryone does. I'm sorry to break it to you but that's not how this game works my friend. " + 
+"If you want your paradise, you better be willing to put in the work. " + 
+"No more half assin it. No more bullshittin yourself and those who give a shit about you. " + 
+"You want your paradise. You better start doing what must be done to get that damn paradise. " + 
+"No more bullshit. Just do it!");
+
+const name = prompt("What is your name young grasshopper?");
+console.log(`That's a beautiful name, ${name}!`);
+
+const goal = prompt("What is it you want to accomplish?");
+const oath = prompt("Are you willing to start putting in the work. Consistently. No more bullshitting. Focus on this and complete it?");
+console.log("Then I hope you stop lying to yourself and breaking your own oath. Do better. Be Better. It's time to become who you were destined to be. It's time to be great.");
